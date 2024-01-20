e03ec7f893e572ddd50564c009e29afbaa5bfd96
bloominolive <bloomin.olive88@gmail.com>
2024-01-19 19:29:03 -0500

    changed from tailwind to bootstrap

---
+++ handcrafted-haven/app/page.js
@@ -0,0 +1,10 @@
+import '../styles/custom-bootstrap.scss';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1 className="text-primary">Hello, Bootstrap!</h1>
+      <button className="btn btn-success">Click me</button>
+    </div>
+  );
+}
