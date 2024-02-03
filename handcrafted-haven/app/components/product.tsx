c2a8f3002bfb95d0fd5fd04308775d870cf92ec7
bloominolive <bloomin.olive88@gmail.com>
2024-02-03 08:10:25 -0500

    adjusted home page styling

--- handcrafted-haven/app/components/product.tsx
+++ handcrafted-haven/app/components/product.tsx
@@ -9,12 +9,10 @@ interface ProductProps {
       imageUrl: string;
     };
   }
-  
+
   const Product: React.FC<ProductProps> = ({ product }) => {
     return (
-      // Adjusting to match the seller-card styling with Tailwind CSS
       <div className="bg-[#feffea] border border-gray-300 rounded-lg p-4 m-4 max-w-sm text-center shadow-lg">
-        {/* Tailwind CSS for image styling to match the sellers' card style */}
         <img src={product.imageUrl} alt={product.name} className="w-full rounded-full mb-2 mx-auto max-w-xs" />
         <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
         <p className="text-sm text-gray-600 mb-4">{product.description}</p>
