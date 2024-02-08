25a2d49347558de1e2ed10834268464163bb2b19
bloominolive <bloomin.olive88@gmail.com>
2024-02-08 15:00:29 -0500

    login styling update

--- handcrafted-haven/app/components/handcraftedhavenlogo.tsx
+++ handcrafted-haven/app/components/handcraftedhavenlogo.tsx
@@ -1,6 +1,8 @@
-import Image from 'next/image';
+import Link from "next/link";
 export default function HandcraftedHavenLogo() {
   return (
-    <Image src={'/images/hhHeader.png'} alt="logo" width={350} height={35} style={{width:"auto", height: "auto"}}/>
+    <div>
+      <Link href="/"><img src="./images/hhHeader.png" className="h-32 rounded-lg flex-shrink-0" /></Link>
+    </div>
   );
 }
