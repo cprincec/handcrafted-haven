671a81d1906472df1e554aa4cb8375589a62348b
bloominolive <bloomin.olive88@gmail.com>
2024-02-09 09:39:48 -0500

    removing testing code

--- handcrafted-haven/app/components/products/product-list.tsx
+++ handcrafted-haven/app/components/products/product-list.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ProductCard from './product-card';
 import { fetchProducts } from '../../lib/data';
-import { Product } from '@/app/lib/definitions';
 
 async function ProductList({ maxPrice }: { maxPrice: string }) {
-  console.log(maxPrice);
   const products = await fetchProducts(maxPrice);
   return (
     <div className="grid grid-cols-1 gap-16 p-12 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
