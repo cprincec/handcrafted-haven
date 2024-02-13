e1fba3eb8656114a2781d1a9f9e73f3951ccab84
cprincec <chukwuprincec@gmail.com>
2024-02-13 14:31:25 +0100

    removed console.logs

--- handcrafted-haven/app/components/nav-wrapper.tsx
+++ handcrafted-haven/app/components/nav-wrapper.tsx
@@ -3,7 +3,6 @@ import Navbar from './navbar';
 
 export default async function NavBarWrapper() {
   const session = await auth();
-  console.log(session);
 
   return <Navbar loggedInUser={session?.user ? session.user : null} />;
 }
